f53932e9e4f043e43aafbc1928a4abcd658a00f3
Wolfgang Wiese <xwolf@xwolf.de>
2013-03-08 12:41:31 +0100

    1.0.3 with backend-functions. plugin will get a new name cause it works now as generic protest plugin

--- protest.php
+++ protest.php
@@ -1,17 +1,31 @@
 <?php
 /*
-Plugin Name: Worldday 2013 against Cyber-Censorship 
-Plugin URI: http://piratenkleider.xwolf.de/plugins/anticensorship-worldday2013
-Description: Black Day Plugin for the world day against cyber censorship 2013: Simple Plugin which enables a dark colored notice for the world day against cyber-censorship 2013 (between hours: 8 - 20). Read nore on:  http://www.reporter-ohne-grenzen.de. Plugin was made by the Pirate Party Germany to use in wordpress related sites. 
-Tags:  Internet Censorship, Worldday 2013, cyber censorship
-Plugin URI: https://github.com/xwolfde/xwolf-anticensorship-worldday2013
-Version: 1.0.2
+Plugin Name: xwolf Protest Page
+Plugin URI: http://piratenkleider.xwolf.de/plugins/xwolf-protest-page
+Description: Black Day Plugin for protest days like the world day against cyber censorship 2013. Plugin enables a dark colored notice. 
+Tags:  Internet Censorship, Worldday 2013, cyber censorship, protest, black day
+Plugin URI: https://github.com/xwolfde/xwolf-protest-page
+Version: 1.0.3
 Author: xwolf
 Author URI: http://blog.xwolf.de
-License: GPL2
+License: GPL3
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
+/*  Copyright 2013  xwolf  ( http://blog.xwolf.de)
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/    
 
 
 /**
@@ -23,30 +37,52 @@ define("XW_PROTEST_TITLE", 'Welttag gegen Internetzensur <br>12.03.2013');
 define("XW_PROTEST_LONGTEXT", 'Wir zeigen uns solidarisch mit allen durch Überwachung und Zensur eingeschränkten Journalisten und Aktivisten weltweit. Die Organisationen Reporter ohne Grenzen und die Piratenpartei rufen am Welttag gegen Internetzensur zu Protesten auf. ');
 define("XW_PROTEST_MORE", 'Weitere Informationen bei <a href="http://www.reporter-ohne-grenzen.de/">Reporter ohne Grenzen</a>. <a href="http://wiki.piratenpartei.de/Welttag-gegen-Internetzensur-2013">Informationen, sowie Plugins und Banner</a> zur Teilnahme finden sich auf dem Wiki der Piratenpartei Deutschland.');
 
-define("XW_PROTEST_usecookie", true);
+define("XW_PROTEST_usecookie", false);
 define("XW_PROTEST_cookiename", 'seen_worldday2013');
 
 define("XW_TESTMODE", false);
-define("XW_PROTEST_year", '2013');
-define("XW_PROTEST_month", '03');
-define("XW_PROTEST_day", '12');
-define("XW_PROTEST_date", '2013-03-12');
+define("XW_PROTEST_date", '2013-03-08');
 define("XW_PROTEST_timestart",  8);
 define("XW_PROTEST_timeend",  20);
 define("XW_PROTEST_timezone", null);
 
+/**
+ * Retrieve the options
+ */
+function xw_get_options() {
+	$xw_protest_opts = get_option( 'xw_protest', XW_PROTEST_date );
+	if ( ! is_array( $xw_protest_opts ) )
+		$xw_protest_opts = array( 'protest_date' => $xw_protest_opts );
+	
+	$xw_protest_opts = array_merge( array(
+		'protest_date'	    => XW_PROTEST_date,
+		'protest_timestart' => XW_PROTEST_timestart,
+		'protest_timeend'   => XW_PROTEST_timeend,
+		'protest_timezone'  => XW_PROTEST_timezone,
+		'usecookie'	    => XW_PROTEST_usecookie,
+		'cookiename'        => XW_PROTEST_cookiename,
+		'mainurl'	    => XW_PROTEST_URL,
+		'image'		    => XW_PROTEST_IMAGE,
+		'title'		    => XW_PROTEST_TITLE,
+		'longtext'	    => XW_PROTEST_LONGTEXT,
+		'moretext'	    => XW_PROTEST_MORE,
+	), $xw_protest_opts );
+	
+	return $xw_protest_opts;
+}
 
 
 function xw_protest_init() {
 	$xw_protest_path = plugin_dir_url( __FILE__ );
-        
+        $xw_opts = xw_get_options();
+	
 	 if (xw_protest_checkdate()) {
 	    if ( xw_see_check() ) { 				
 		    wp_register_style( 'xw_protest_css', $xw_protest_path . 'protest.css' );
 		    wp_enqueue_style( 'xw_protest_css' );
 		    add_action('wp_footer', 'xw_protest_footercode');
-                    if ( XW_PROTEST_usecookie )
-				setcookie( XW_PROTEST_cookiename, 1, 0, '/' );
+                    if ( $xw_opts['usecookie'] )
+				setcookie( $xw_opts['cookiename'], 1, 0, '/' );
 		
 	    } 
 	 }
@@ -56,29 +92,30 @@ add_action( 'init', 'xw_protest_init' );
 
 
 function xw_see_check() {
-    
+    $xw_opts = xw_get_options();
     if (is_admin()) {
         return false;
     }    
     if (XW_TESTMODE) {
 	    return true;
-	}
-    if ( isset( $_COOKIE ) && array_key_exists( XW_PROTEST_cookiename, $_COOKIE ) && (XW_PROTEST_usecookie) ) {		
-		return false;
-     }
+    }
+    if ( isset( $_COOKIE ) && array_key_exists( $xw_opts['cookiename'], $_COOKIE ) && ($xw_opts['usecookie']) ) {		
+	    return false;
+    }
 
     return true;
 }
 function xw_protest_checkdate(){
+     $xw_opts = xw_get_options();
 	if(XW_PROTEST_timezone){
-		date_default_timezone_set(XW_PROTEST_timezone);
+		date_default_timezone_set($xw_opts['protest_timezone']);
 	}
 	$toreturn = false;
 	if (XW_TESTMODE) {
 	    return true;
 	}
-	if(date('Y-m-d')==XW_PROTEST_year.'-'.sprintf('%02d',XW_PROTEST_month).'-'.sprintf('%02d',XW_PROTEST_day)){
-		if(date('H')>=XW_PROTEST_timestart && date('H')<XW_PROTEST_timeend){
+	if(date('Y-m-d')== $xw_opts['protest_date']){
+		if(date('H')>=$xw_opts['protest_timestart'] && date('H')<$xw_opts['protest_timeend']){
 			$toreturn = true;
 		}
 	}
@@ -87,34 +124,40 @@ function xw_protest_checkdate(){
 
 
 function xw_protest_footercode() {
+    $xw_opts = xw_get_options();
     $xw_protest_path = plugin_dir_url( __FILE__ );
 	echo '<script type="text/javascript">
 	/* <![CDATA[ */';
 	echo '$(function () {  $(\'body\').append($(
         \'<div id=\"protest\"><div><a href=\"#\" class=\"close\" tabindex=\"1\">X</a>\' + ';
-	if (XW_PROTEST_IMAGE) {
+	if ($xw_opts['image']) {
 	    echo '\'<p>';
-	    if (XW_PROTEST_URL) { echo '<a class=\"link\" href=\"'.XW_PROTEST_URL.'\">'; }
+	    if ($xw_opts['mainurl']) { echo '<a class=\"link\" href=\"'.$xw_opts['mainurl'].'\">'; }
 	    echo '<img src=\"';
-	    echo $xw_protest_path.XW_PROTEST_IMAGE.'\"';
-	    if (XW_PROTEST_TITLE) { echo ' alt=\"'.XW_PROTEST_TITLE.'\"'; }
-	    if (XW_PROTEST_LONGTEXT) { echo ' longdesc=\"'.XW_PROTEST_LONGTEXT.'\"'; }
+	    if (strpos($xw_opts['image'], 'http') === false) {
+		echo $xw_protest_path;
+	    } 
+	    echo $xw_opts['image'];	
+	    
+	    echo '\"';
+	    if ($xw_opts['title']) { echo ' alt=\"'.$xw_opts['title'].'\"'; }
+	    if ($xw_opts['longtext']) { echo ' longdesc=\"'.$xw_opts['longtext'].'\"'; }
 	    echo '>';
-	    if (XW_PROTEST_URL) { echo '</a>'; }
+	    if ($xw_opts['mainurl']) { echo '</a>'; }
 	    echo '</p>\' + ';
 	} else {
-	    if (XW_PROTEST_TITLE) { 
+	    if ($xw_opts['title']) { 
 		echo '\'<h1>';
-		if (XW_PROTEST_URL) { echo '<a class=\"link\" href=\"'.XW_PROTEST_URL.'\">'; }
-		echo XW_PROTEST_TITLE;
-		if (XW_PROTEST_URL) { echo '</a>'; }
+		if ($xw_opts['mainurl']) { echo '<a class=\"link\" href=\"'.$xw_opts['mainurl'].'\">'; }
+		echo $xw_opts['title'];
+		if ($xw_opts['mainurl']) { echo '</a>'; }
 		echo '</h1>'; 
 		echo '\' + ';
 	    }	    
-	    if (XW_PROTEST_LONGTEXT) { echo '\'<p>'.XW_PROTEST_LONGTEXT.'</p>\' + '; } 
+	    if ($xw_opts['longtext']) { echo '\'<p>'.$xw_opts['longtext'].'</p>\' + '; } 
 	}
 	
-        if (XW_PROTEST_MORE) { echo '\'<p class=\"more\">'.XW_PROTEST_MORE.'</p>\' + '; } 
+        if ($xw_opts['moretext']) { echo '\'<p class=\"more\">'.$xw_opts['moretext'].'</p>\' + '; } 
             
         echo '\'</div>';
         echo '</div>\' )); ';
@@ -141,6 +184,209 @@ $(window).bind(\'resize\', function(){
       </script> ';
 }
 
+
+
+/**
+ * Add options page to the administration menu
+ */
+function xw_add_options_page() {
+	add_submenu_page( 'options-general.php',
+		__( 'Protest Page Options' , 'xw-protest-plugin'),
+		__( 'Protest Page Options' , 'xw-protest-plugin'), 
+		'manage_options', 'xw_options_page', 'xw_options_page_callback' );
+	add_action( 'admin_init', 'register_xw_options' );
+}
+add_action( 'admin_menu', 'xw_add_options_page' );
+
+/**
+ * Whitelist the SOPA options and set up the options page
+ */
+function register_xw_options() {
+	register_setting( 'xw_options_page', 'xw_protest', 'sanitize_xw_opts' );
+	add_settings_section( 'xw_options_section', 
+		__( 'Protest Page' , 'xw-protest-plugin'), 'xw_options_section_callback', 'xw_options_page' );
+	
+	add_settings_field( 'protest_date', __( 'Blackout date:' , 'xw-protest-plugin'),
+		'xw_options_field_callback', 'xw_options_page', 'xw_options_section', 
+		array( 'label_for' => 'xw_protest_date', 'field_name' => 'protest_date' ) );
+
+	add_settings_field( 'protest_timestart', __( 'Start time:' , 'xw-protest-plugin'),
+		'xw_options_field_callback', 'xw_options_page', 'xw_options_section', 
+		array( 'label_for' => 'xw_protest_timestart', 'field_name' => 'protest_timestart' ) );
+
+	add_settings_field( 'protest_timeend', __( 'Ending time:' , 'xw-protest-plugin'),
+		'xw_options_field_callback', 'xw_options_page', 'xw_options_section', 
+		array( 'label_for' => 'xw_protest_timeend', 'field_name' => 'protest_timeend' ) );
+
+	add_settings_field( 'usecookie', 
+		__( 'Use cookies' ,'xw-protest-plugin'), 
+		'xw_options_field_callback', 'xw_options_page', 'xw_options_section',
+		array( 'label_for' => 'xw_usecookie', 'field_name' => 'usecookie' ) );
+
+	add_settings_field( 'mainurl', 
+		__( 'Link to an information page' , 'xw-protest-plugin'),
+		'xw_options_field_callback', 'xw_options_page', 'xw_options_section',
+		array( 'label_for' => 'xw_mainurl', 'field_name' => 'mainurl' ) );
+
+	add_settings_field( 'image', 
+		__( 'Image' , 'xw-protest-plugin'),
+		'xw_options_field_callback', 'xw_options_page', 'xw_options_section',
+		array( 'label_for' => 'xw_image', 'field_name' => 'image' ) );
+
+	add_settings_field( 'title', 
+		__( 'Title' , 'xw-protest-plugin'),
+		'xw_options_field_callback', 'xw_options_page', 'xw_options_section',
+		array( 'label_for' => 'xw_title', 'field_name' => 'title' ) );
+
+	add_settings_field( 'longtext', 
+		__( 'Information text' , 'xw-protest-plugin'),
+		'xw_options_field_callback', 'xw_options_page', 'xw_options_section',
+		array( 'label_for' => 'xw_longtext', 'field_name' => 'longtext' ) );
+	
+	add_settings_field( 'moretext', 
+		__( 'Additional information text' , 'xw-protest-plugin'),
+		'xw_options_field_callback', 'xw_options_page', 'xw_options_section',
+		array( 'label_for' => 'xw_moretext', 'field_name' => 'moretext' ) );	
+}
+
+/**
+ * Sanitize the updated SOPA options
+ * @param array $input the value of the options
+ * @return array the sanitized values
+ */
+function sanitize_xw_opts( $input ) {
+	$input['protest_date'] = esc_attr( stripslashes( $input['protest_date'] ) );
+	$input['usecookie'] = array_key_exists( 'usecookie', $input ) && ( '1' === $input['usecookie'] || 1 === $input['usecookie'] ) ? 1 : 0;
+	$input['mainurl'] = esc_attr( stripslashes( $input['mainurl'] ) );
+	$input['image'] = esc_attr( stripslashes( $input['image'] ) );
+	$input['title'] = stripslashes(  $input['title']  );
+	$input['longtext'] =   stripslashes($input['longtext'] );
+	$input['moretext'] =   stripslashes($input['moretext'] );
+	$input['protest_timeend'] = (int)$input['protest_timeend'];
+	
+	if ($input['protest_timeend']>23) {
+	    $input['protest_timeend'] =23;
+	}
+	$input['protest_timestart'] = (int)$input['protest_timestart'];
+	if (($input['protest_timestart']>23) || ($input['protest_timestart']>$input['protest_timeend'])) {
+	    $input['protest_timestart'] =0;
+	}
+	$input['cookiename'] = XW_PROTEST_cookiename . '_'. md5( time() );
+	
+	return $input;
+}
+
+/**
+ * Output the options page HTML
+ */
+function xw_options_page_callback() {
+	if ( ! current_user_can( 'manage_options' ) )
+		wp_die( 'You do not have sufficient permissions to view this page.' );
+?>
+<div class="wrap">
+	<h2><?php _e( 'xwolf Protest Page Options' , 'xw-protest-plugin') ?></h2>
+    <form method="post" action="options.php">
+    <?php settings_fields( 'xw_options_page' ) ?>
+    <?php do_settings_sections( 'xw_options_page' ) ?>
+    <p><input type="submit" class="button-primary" value="<?php _e( 'Save Changes' , 'xw-protest-plugin') ?>"/></p>
+    </form>
+</div>
+<?php
+}
+
+/**
+ * Output the message to be displayed at the top of the options section
+ */
+function xw_options_section_callback() {
+	_e( '<p>Please choose the date on which you would like the notice for protest to occur.</p>' , 'xw-protest-plugin');
+	_e( '<p><em>Saving these options will reset all of the  cookies, so visitors will see the message again even if they have already seen it.</em></p>' , 'xw-protest-plugin');
+}
+
+/**
+ * Output the HTML for the options form elements
+ */
+function xw_options_field_callback( $args ) {
+	$xw_opts = xw_get_options();
+	
+	switch ( $args['field_name'] ) {
+		case 'protest_date':
+?>
+	<input class="widefat" type="text" value="<?php echo $xw_opts['protest_date'] ?>" 
+	       name="xw_protest[protest_date]" id="xw_protest_date"/><br />
+<em><?php _e( 'Please enter protest-date in YYYY-MM-DD format.' , 'xw-protest-plugin') ?></em>
+<?php
+		break;
+		
+		case 'usecookie':
+?>
+	<input type="checkbox" name="xw_protest[usecookie]" id="xw_usecookie" 
+	       value="1"<?php checked( 1, $xw_opts['usecookie'] ) ?>/><br />
+<em><?php _e( 'By default, after a visitor has seen the message, all other visits 
+    to your site will show the regular content. 
+    If you check the box above, they will see the message every time they 
+    visit your site (as long as it\'s active).' , 'xw-protest-plugin') ?></em>
+<?php
+		break;
+		case 'protest_timestart':
+?>
+<input type="text" length="2" maxlength="2" value="<?php echo $xw_opts['protest_timestart'] ?>" 
+	       name="xw_protest[protest_timestart]" id="xw_protest_timestart"/><br />
+<em><?php _e( 'Please enter the time for starting protest on the protest date (hour number).' , 'xw-protest-plugin') ?></em>
+<?php
+
+
+		
+		break;
+		case 'protest_timeend':
+?>
+<input type="text" length="2" maxlength="2" value="<?php echo $xw_opts['protest_timeend'] ?>" 
+	       name="xw_protest[protest_timeend]" id="xw_protest_timeend"/><br />
+<em><?php _e( 'Please enter the time for ending protest on the protest date (hour number).' , 'xw-protest-plugin') ?></em>
+<?php
+		break;
+		case 'mainurl':
+?>
+<input class="widefat" type="text" value="<?php echo $xw_opts['mainurl'] ?>" 
+	       name="xw_protest[mainurl]" id="xw_mainurl"/><br />
+<em><?php _e( 'Optional URL for information page to link to.' , 'xw-protest-plugin') ?></em>
+<?php
+
+		break;
+		case 'image':
+?>
+<input class="widefat" type="text" value="<?php echo $xw_opts['image'] ?>" 
+	       name="xw_protest[image]" id="xw_image"/><br />
+<em><?php _e( 'Optional Image (banner or logo). if starts with http it will be external, otherwise it must residue in plugin directory' , 'xw-protest-plugin') ?></em>
+<?php
+
+		break;
+		case 'title':
+?>
+<input class="widefat" type="text" value="<?php echo $xw_opts['title'] ?>" 
+	       name="xw_protest[title]" id="xw_title"/><br />
+<em><?php _e( 'Title for protest notice (if an image is active as banner, used as alt-attribute' , 'xw-protest-plugin') ?></em>
+<?php
+		break;
+		case 'longtext':
+?>
+<textarea class="widefat"  name="xw_protest[longtext]" id="xw_longtext"/><?php echo esc_textarea( $xw_opts['longtext']) ?></textarea> 
+	       <br />
+<em><?php _e( 'Short information text about protest' , 'xw-protest-plugin') ?></em>
+<?php
+		break;
+		case 'moretext':
+?>
+<textarea class="widefat"  name="xw_protest[moretext]" id="xw_moretext"/><?php echo esc_textarea( $xw_opts['moretext']) ?></textarea> 
+	       <br />
+<em><?php _e( 'Additional informations for protest' , 'xw-protest-plugin') ?></em>
+<?php
+		
+		break;	    
+	}
+}
+
+
+
 /**
  * Install or update plugin
  */
@@ -153,6 +399,7 @@ function xw_protest_install() {
  */
 function xw_protest_uninstall() {
     /* Nothing yet */
+    delete_option( 'xw_protest' );
     
 }
 
--- readme.txt
+++ readme.txt
@@ -1,37 +1,40 @@
-=== xwolf Anti-Censorship Worldday 2013 ===
-Plugin Name: xwolf Anti-Censorship Worldday 2013
-Plugin URI: https://github.com/xwolfde/xwolf-anticensorship-worldday2013
+=== xwolf Protest Page ===
+Plugin Name: xwolf Protest Page
+Plugin URI: http://piratenkleider.xwolf.de/plugins/xwolf-protest-page
+Description: Black Day Plugin for protest days like the world day against cyber censorship 2013. Plugin enables a dark colored notice. 
+Tags:  Internet Censorship, Worldday 2013, cyber censorship, protest, black day
+Plugin URI: https://github.com/xwolfde/xwolf-protest-page
 Donate link: https://flattr.com/donation/give/to/xwolf
-Version: 1.0.1
+Version: 1.0.3
 Author: xwolf
 Author URI: http://blog.xwolf.de
-License: GPL2
+License: GPL3
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Tags:  Internet Censorship, Worldday 2013
 Requires at least: 3.3
 Tested up to: 3.5
 Stable tag: trunk
 
-Plugin enables to go dark on the world day against cyber-censorship 2013 
+Plugin enables to go dark on protest days, like the world day against cyber-
+censorship 2013 
 
 == Description ==
 
-Displays a black day with a notice about the world day against 
-cyber-censorship 2013 as invoked by Reporter Without Borders,
-http://en.rsf.org/ .
-It wont forbit the access to the website. Users can close it and continue reading.
-
-This plugin displays a german text/picture. This can be changed with constants 
-in the source code yet. There is no admin page in this version.
+Displays a black day with a notice. 
+As default it shows a (german) protest notice by the Pirate Party Germany 
+about the world day against cyber-censorship 2013 (as invoked by Reporter 
+Without Borders, http://en.rsf.org/).
+It wont forbit the access to the website. Users can close it and continue 
+reading.
 
 A cookie will be set for users after they saw the notice the first time. This
 will prevent them to get spamed by the notice every time they come back
 at the same day.
 
+Within the backend options this plugin can be used for other protest days also.
+
+
 == Credits ==
 * First Design / JavaScript by @pakki
-* Wordpress-Plugin by xwolf
-
 
 = Support =
 * By [Flattr](https://flattr.com/donation/give/to/xwolf)
@@ -43,6 +46,7 @@ at the same day.
 == Screenshots ==
 
 1. Default view which is displayed on 2013/03/12
+2. Backend
 
 == Installation ==
 
